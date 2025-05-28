e5435cd49170aafe4e4514a22c0ffe67ecc3101d
sebaek <sebaek@gmail.com>
2025-05-28 14:54:15 +0900

    request parameter

--- src/main/java/com/example/spring/controller/Controller02.java
+++ src/main/java/com/example/spring/controller/Controller02.java
@@ -1,4 +1,30 @@
 package com.example.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
 public class Controller02 {
+
+    // /main2/sub1?name
+    @RequestMapping(value = "/main2/sub1", params = "name")
+    public void sub1(WebRequest request) {
+        // header 정보
+        request.getHeader("User-Agent");
+
+
+        System.out.println("Controller02.sub1");
+    }
+
+    // /main2/sub2?name&address
+    // ? : query string
+    // query string : name1=value1&name2=value2
+    @RequestMapping(value = "/main2/sub2", params = {"name", "address"})
+    public void sub2(WebRequest request) {
+
+        System.out.println("Controller02.sub2");
+    }
+
+
 }
