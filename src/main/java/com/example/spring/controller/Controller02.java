0a91526a59c78b9da6b9c2b24f6873ca6b3b111a
sebaek <sebaek@gmail.com>
2025-05-28 15:15:28 +0900

    request parameter

--- src/main/java/com/example/spring/controller/Controller02.java
+++ src/main/java/com/example/spring/controller/Controller02.java
@@ -26,5 +26,20 @@ public class Controller02 {
         System.out.println("Controller02.sub2");
     }
 
+    // /main2/sub3?name=musk
+    @RequestMapping(value = "/main2/sub3", params = "name")
+    public void sub3(WebRequest request) {
+        String name = request.getParameter("name");
+        System.out.println("name = " + name);
+        System.out.println("Controller02.sub3");
+    }
 
+    // /main2/sub3?address=seoul
+    // /main2/sub3?address=jeju
+    @RequestMapping(value = "/main2/sub3", params = "address")
+    public void sub4(WebRequest request) {
+        // address request parameter 를 얻어서 출력하는 코드
+        
+        System.out.println("Controller02.sub4");
+    }
 }
