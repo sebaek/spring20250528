d5a7de695abb0e96bfcbbc210b906093dafbe8f3
sebaek <sebaek@gmail.com>
2025-05-30 14:39:49 +0900

    th:attr, th:class, th:attrappend, th:classappend

--- src/main/java/com/example/spring/controller/Controller08.java
+++ src/main/java/com/example/spring/controller/Controller08.java
@@ -1,13 +1,24 @@
 package com.example.spring.controller;
 
+import com.example.spring.dto.MyBean053;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("main8")
 public class Controller08 {
     @RequestMapping("sub1")
-    public String sub1(){
+    public String sub1(Model model) {
+
+        model.addAttribute("students",
+                List.of(new MyBean053(10, 33.3, "trump", true),
+                        new MyBean053(20, 44.4, "donald", false),
+                        new MyBean053(30, 55.5, "musk", true),
+                        new MyBean053(40, 66.6, "elvis", false),
+                        new MyBean053(50, 77.7, "elon", true)));
 
 
         return "main8/sub1";
