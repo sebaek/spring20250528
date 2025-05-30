4d59c60c5b9228fac114b95fa132bbad8c895785
sebaek <sebaek@gmail.com>
2025-05-30 14:43:39 +0900

    th:class, th:classappend

--- src/main/java/com/example/spring/controller/Controller08.java
+++ src/main/java/com/example/spring/controller/Controller08.java
@@ -1,6 +1,7 @@
 package com.example.spring.controller;
 
 import com.example.spring.dto.MyBean053;
+import com.example.spring.dto.MyBean57;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,28 @@ public class Controller08 {
 
 
         return "main8/sub1";
+    }
+
+    @RequestMapping("sub2")
+    public String sub2(Model model) {
+
+        /*
+        가격은 italic 체로 출력
+        5000원 이상은 굵은 글씨로
+
+        |이름|생일 |가격|
+        |lee|01-01|5000|
+
+         */
+
+        model.addAttribute("myList", List.of(
+                new MyBean57("lee", "01-01", 5000),
+                new MyBean57("kim", "02-02", 4000),
+                new MyBean57("son", "03-03", 6000),
+                new MyBean57("park", "04-04", 3000),
+                new MyBean57("choi", "05-05", 7000)));
+
+        return "main8/sub2";
     }
 
 }
