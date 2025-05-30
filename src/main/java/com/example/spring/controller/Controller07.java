fbf67b38bec06098b1515aab126ad1cb930580de
sebaek <sebaek@gmail.com>
2025-05-30 09:30:44 +0900

    th:each 와 loop의 상태

--- src/main/java/com/example/spring/controller/Controller07.java
+++ src/main/java/com/example/spring/controller/Controller07.java
@@ -1,5 +1,6 @@
 package com.example.spring.controller;
 
+import com.example.spring.dto.MyBean57;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +17,26 @@ public class Controller07 {
         model.addAttribute("aiList", List.of("gpt", "gemini", "grok", "deep"));
 
         return "main7/sub1";
+    }
+
+    @RequestMapping("sub2")
+    public String sub2(Model model) {
+        /*
+        |번호|이름|생일|가격|
+        |1  |    |   |   |
+        |2  |    |   |   |
+        |3  |    |   |   |
+        |4  |    |   |   |
+        |5  |    |   |   |
+         */
+
+        model.addAttribute("myList", List.of(
+                new MyBean57("lee", "01-01", 5000),
+                new MyBean57("kim", "02-02", 4000),
+                new MyBean57("son", "03-03", 6000),
+                new MyBean57("park", "04-04", 3000),
+                new MyBean57("choi", "05-05", 7000)));
+
+        return "main7/sub2";
     }
 }
