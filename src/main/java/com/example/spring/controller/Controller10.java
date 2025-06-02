280d77a2d16475bd1b5e537714ef9241b15091d1
sebaek <sebaek@gmail.com>
2025-06-02 14:13:58 +0900

    get, post

--- src/main/java/com/example/spring/controller/Controller10.java
+++ src/main/java/com/example/spring/controller/Controller10.java
@@ -2,6 +2,7 @@ package com.example.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("main10")
@@ -16,7 +17,26 @@ public class Controller10 {
     public String sub2(String name, String city) {
         System.out.println("name = " + name);
         System.out.println("city = " + city);
-        
+
         return "main10/sub1";
     }
+
+    // get 요청 처리
+    @RequestMapping(value = "sub3", method = RequestMethod.GET)
+    public String sub3(String keyword) {
+        System.out.println("keyword = " + keyword);
+        System.out.println("Controller10.sub3");
+
+        return "main10/sub3";
+    }
+
+    // post 요청 처리
+    @RequestMapping(value = "sub3", method = RequestMethod.POST)
+    public String sub4(String address) {
+        System.out.println("address = " + address);
+        System.out.println("Controller10.sub4");
+
+        return "main10/sub3";
+    }
+
 }
