a32ad4a89f254408f502823a607f6e12de67eca9
sebaek <sebaek@gmail.com>
2025-06-11 15:33:30 +0900

    alter table

--- sql/19alter.sql
+++ sql/19alter.sql
@@ -14,8 +14,8 @@ VALUES (5, 'lee');
 
 # ALTER TABLE : 테이블 수정
 # 컬럼 추가
-# 컬럼 TYPE 제약사항 변경
 # 컬럼명 변경
+# 컬럼 TYPE 제약사항 변경
 
 ALTER TABLE table22
     ADD COLUMN col3 DATETIME NOT NULL DEFAULT NOW();
@@ -43,4 +43,60 @@ ALTER TABLE table22
 
 # 연습
 # table22 에 새로운 컬럼 2개 추가
+ALTER TABLE table22
+    ADD COLUMN col11 VARCHAR(10) NOT NULL DEFAULT '없음';
+ALTER TABLE table22
+    ADD COLUMN col12 INT NOT NULL DEFAULT 100;
+
+
+# 컬럼명 변경
+CREATE TABLE table23
+(
+    col1 INT,
+    col2 VARCHAR(10)
+);
+ALTER TABLE table23
+    CHANGE COLUMN col1 col11 INT;
+DESC table23;
+
+ALTER TABLE table23
+    CHANGE COLUMN col11 col21 INT NOT NULL;
+
+ALTER TABLE table23
+    CHANGE COLUMN col21 col31 VARCHAR(10) NOT NULL;
+
+ALTER TABLE table23
+    MODIFY COLUMN col31 INT NOT NULL UNIQUE;
+
+# 연습
+# table23 col2 의 타입을 DEC(10, 2), NOT NULL, UNIQUE 변경
+ALTER TABLE table23
+    MODIFY COLUMN col2 DEC(10, 2) NOT NULL UNIQUE;
+
+CREATE TABLE table24
+(
+    col1 VARCHAR(5),
+    col2 BIGINT
+);
+INSERT INTO table24
+    (col1, col2)
+VALUES ('abcde', 4000000000);
+INSERT INTO table24
+    (col1, col2)
+VALUES ('abcde', 4000000000);
+SELECT *
+FROM table24;
+ALTER TABLE table24
+    MODIFY COLUMN col1 VARCHAR(3);
+ALTER TABLE table24
+    MODIFY COLUMN col1 VARCHAR(10);
+
+ALTER TABLE table24
+    MODIFY COLUMN col1 VARCHAR(10) UNIQUE;
+
+ALTER TABLE table24
+    MODIFY COLUMN col2 INT;
+
+ALTER TABLE table24
+    MODIFY COLUMN col2 BIGINT UNIQUE;
 
