87d299be9ff9a4dd344dd5a2c6ee424656d112b6
sebaek <sebaek@gmail.com>
2025-06-12 11:41:48 +0900

    join

---
+++ sql/24join.sql
@@ -0,0 +1,82 @@
+USE mydatabase;
+
+CREATE TABLE table38
+(
+    id INT
+);
+INSERT INTO table38
+    (id)
+VALUES (9),
+       (8),
+       (7);
+
+SELECT *
+FROM table34;
+SELECT *
+FROM table35;
+SELECT *
+FROM table38;
+
+SELECT *
+FROM table34 t34
+         JOIN table35 t35
+         JOIN table38 t38;
+
+SELECT *
+FROM w3schools.Products p
+         JOIN w3schools.Categories c
+         JOIN w3schools.Suppliers s
+WHERE p.CategoryID = c.CategoryID
+  AND p.SupplierID = s.SupplierID;
+
+SELECT *
+FROM w3schools.Products p
+         JOIN w3schools.Categories c
+              ON p.CategoryID = c.CategoryID
+         JOIN w3schools.Suppliers s
+              ON p.SupplierID = s.SupplierID;
+
+# 카테고리별 공급자 정보
+SELECT c.CategoryName, s.SupplierName
+FROM w3schools.Products p
+         JOIN w3schools.Categories c
+              ON p.CategoryID = c.CategoryID
+         JOIN w3schools.Suppliers s
+              ON p.SupplierID = s.SupplierID
+ORDER BY c.CategoryName;
+
+# 공급자별 공급하는 상품 카테고리
+SELECT DISTINCT s.SupplierName, c.CategoryName
+FROM w3schools.Products p
+         JOIN w3schools.Categories c
+              ON p.CategoryID = c.CategoryID
+         JOIN w3schools.Suppliers s
+              ON p.SupplierID = s.SupplierID
+ORDER BY s.SupplierName;
+
+SELECT DISTINCT s.ShipperName, c.CustomerName
+FROM w3schools.Orders o
+         JOIN w3schools.Customers c
+              ON o.CustomerID = c.CustomerID
+         JOIN w3schools.Shippers s
+              ON o.ShipperID = s.ShipperID
+ORDER BY s.ShipperName, c.CustomerName;
+
+# 연습
+# 1996-07-05 에 주문된 상품명 목록
+# Orders, OrderDetails, Products
+SELECT o.OrderDate, p.ProductName
+FROM w3schools.Orders o
+         JOIN w3schools.OrderDetails od
+              ON o.OrderID = od.OrderID
+         JOIN w3schools.Products p
+              ON od.ProductID = p.ProductID
+WHERE OrderDate = '1996-07-05'
+ORDER BY p.ProductName;
+
+# 연습
+# Tofu 상품을 어떤 고객이 언제 주문했는지 조회
+# Orders, Products, Customers, OrderDetails
+
+
+
