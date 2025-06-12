aec0e45e35bee4dd46572819723c48fa742a1e8c
sebaek <sebaek@gmail.com>
2025-06-12 15:22:01 +0900

    sub query

---
+++ sql/26subQuery.sql
@@ -0,0 +1,35 @@
+# sub query : 쿼리 안의 쿼리
+USE mydatabase;
+
+# 한 번도 주문된 적 없는 상품 목록
+SELECT p.ProductID, p.ProductName
+FROM w3schools.OrderDetails od
+         RIGHT JOIN w3schools.Products p on od.ProductID = p.ProductID
+WHERE od.OrderDetailID IS NULL
+ORDER BY p.ProductID;
+
+# 1. 주문된 적 있는 상품 ID
+SELECT ProductID
+FROM w3schools.OrderDetails;
+
+# 2. 위 쿼리결과 에 없는 상품 ID 들
+SELECT ProductID, ProductName
+FROM w3schools.Products
+WHERE ProductID NOT IN (SELECT ProductID
+                        FROM w3schools.OrderDetails);
+
+# 상품과 카테고리 정보 조회
+SELECT Products.ProductID, ProductName, CategoryName
+FROM w3schools.Products
+         JOIN w3schools.Categories c on Products.CategoryID = c.CategoryID;
+
+SELECT ProductID,
+       ProductName,
+       (SELECT CategoryName
+        FROM w3schools.Categories c
+        WHERE c.CategoryID = p.CategoryID) CategoryName
+FROM w3schools.Products p;
+
+# 연습
+# 1996-08-01 에 주문 처리한 직원명 조회
+# JOIN, Sub Query
