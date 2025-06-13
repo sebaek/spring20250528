f566443c37b85b9710fada669f72bbe1b46e24a0
sebaek <sebaek@gmail.com>
2025-06-13 15:19:14 +0900

    dependency injection

--- src/main/java/com/example/core/app7/App7.java
+++ src/main/java/com/example/core/app7/App7.java
@@ -8,8 +8,8 @@ public class App7 {
     public static void main(String[] args) {
         var context = SpringApplication.run(App7.class, args);
         Bean2 bean = context.getBean(Bean2.class);
-        // 실행 잘 도록 Bean2 에 setter 만들고
-        // annotaion들 붙이기
+        // 실행 잘 되도록 Bean2 에 setter 만들고
+        // annotation들 붙이기
 
         bean.action();
 
