2612b09f1bf32aafa3925d4e3a894d072df0f0fb
sebaek <sebaek@gmail.com>
2025-06-13 15:50:08 +0900

    dependency injection

---
+++ src/main/java/com/example/core/app11/Bean1.java
@@ -0,0 +1,9 @@
+package com.example.core.app11;
+
+public class Bean1 {
+    final Bean2 bean2;
+
+    public void some() {
+        bean2.crud();
+    }
+}
---
+++ src/main/java/com/example/core/app11/Bean2.java
@@ -0,0 +1,7 @@
+package com.example.core.app11;
+
+public class Bean2 {
+    public void crud() {
+        System.out.println("Bean2.crud");
+    }
+}
