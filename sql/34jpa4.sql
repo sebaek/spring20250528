c86598801fffd7911e0c56f2caba319e783d04ac
sebaek <sebaek@gmail.com>
2025-06-19 14:13:59 +0900

    projection

--- sql/34jpa4.sql
+++ sql/34jpa4.sql
@@ -125,3 +125,39 @@ SELECT ProductName, Unit, ROUND(Price, 0), CategoryID + 1
 FROM w3schools.Products
 Limit 5;
 
+# 연습
+# projection
+
+# OrderInfo interface * 1
+# Entity41 * 1
+# Entity41Repository * 1
+# Entity41Repository.query1() * 1
+# service.action() * 1
+# controller.sub() * 1
+
+# 주문
+CREATE TABLE my_table41
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+    product_id INT,
+    quantity   INT,
+    FOREIGN KEY (product_id) REFERENCES my_table39 (id)
+);
+
+INSERT INTO my_table41
+    (order_date, product_id, quantity)
+VALUES ('1998-01-01', 2, 12),
+       ('1999-02-02', 2, 2),
+       ('2000-03-03', 4, 34),
+       ('2001-04-04', 5, 42),
+       ('2002-05-05', 3, 5),
+       ('2003-06-06', 3, 6);
+SELECT o.order_date, p.name product_name, p.price, o.quantity, c.name category_name
+FROM my_table39 p
+         JOIN my_table40 c
+              ON p.category_id = c.id
+         JOIN my_table41 o
+              ON o.product_id = p.id;
+
+
