333f4ae5d80670195caecd95abce9384b3c8f93b
sebaek <sebaek@gmail.com>
2025-06-19 09:37:50 +0900

    n : n

--- sql/34jpa4.sql
+++ sql/34jpa4.sql
@@ -32,6 +32,41 @@ CREATE TABLE my_table33
 );
 
 
+# 학생
+CREATE TABLE my_table34
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(30),
+    birth_date DATE,
+    info       VARCHAR(30)
+);
+
+# 강의
+CREATE TABLE my_table35
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    lecture_title VARCHAR(30),
+    info          VARCHAR(50),
+    place         VARCHAR(50)
+);
+
+# 학생-강의(수강)
+CREATE TABLE my_table36
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    registered_at DATE,
+    enabled       VARCHAR(1),
+    money         VARCHAR(1),
+    student_id    INT NOT NULL,
+    lecture_id    INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES my_table34 (id),
+    FOREIGN KEY (lecture_id) REFERENCES my_table35 (id)
+);
+
+# 연습
+# 34,35,36 entity 만들기
+# 1. intellij 도움으로 만들기
+# 2. 지우고, 직접 만들기
 
 
 
