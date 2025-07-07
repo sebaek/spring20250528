86a1f6880f40430f2eca10d5f9a7cc56a35cdbcd
sebaek <sebaek@gmail.com>
2025-07-07 15:41:57 +0900

    put, delete 방식 받기

---
+++ src/main/java/com/example/spring/controller/Controller30.java
@@ -0,0 +1,23 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("/api/main30")
+public class Controller30 {
+    //    @RequestMapping(method = RequestMethod.PUT, path = "sub1")
+    @PutMapping("sub1")
+    public String sub1() {
+        System.out.println("Controller30.sub1");
+        return null;
+    }
+
+    //    @RequestMapping(method = RequestMethod.DELETE, path = "sub2")
+    @DeleteMapping("sub2")
+    public String sub2() {
+        System.out.println("Controller30.sub2");
+        return null;
+    }
+}
